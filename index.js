b8e1c25b5e4201a023bc01db4f5ae967d49a7e1c
Kyoz <banminkyoz@gmail.com>
2018-07-13 17:50:29 +0700

    Update script

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const inspireQuotes = require('./quotes/inspire');
-const developerQuotes = require('./quotes/developer');
 const loveQuotes = require('./quotes/love');
+const developerQuotes = require('./quotes/developer');
 
-exports.getAll = () => {
-  return [].concat(inspireQuotes, developerQuotes, loveQuotes);
+const randomFrom = array => array[Math.floor(Math.random() * array.length)];
+
+const quotes = new Map()
+  .set('inspire', inspireQuotes)
+  .set('love', loveQuotes)
+  .set('developer', developerQuotes)
+  .set('all', [...inspireQuotes, ...developerQuotes, ...loveQuotes]);
+
+exports.all = type => quotes.has(type) ? quotes.get(type) : quotes.get('all');
+
+exports.random = type => {
+  return type ? randomFrom(quotes.get(type)) : randomFrom(quotes.get('all'));
 };
 
-console.log(this.getAll());
+console.log(this.all('love').length)
